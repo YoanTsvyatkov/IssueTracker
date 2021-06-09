db055c32b2a403dd7eeb728912722be87ae589fd
pgospoding <plamenna99@abv.bg>
2021-06-09 21:30:52 +0300

    add edit project

--- client/script/project.js
+++ client/script/project.js
@@ -1,6 +1,12 @@
 const projectForm = document.getElementById('project-form');
 const projectList = document.getElementById('project-list-container');
 
+const projectEditForm = document.getElementById('edit-project');
+console.log(projectEditForm);
+
+
+
+
 projectForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     
@@ -20,6 +26,7 @@ projectForm.addEventListener('submit', async (event) => {
         console.error(error);
     }
 });
+
 
 async function displayProjects(){
     try{
@@ -61,17 +68,28 @@ function addProject(project){
     deleteButton.innerHTML = "Delete";
     deleteButton.className = "btn btn-danger";
 
+    const issueButton = document.createElement("button");
+    issueButton.id = "btn-issue";
+    issueButton.innerHTML = "Issue";
+
+    
+
     const box = document.createElement("div");
     box.id = "box"
     box.appendChild(editButton);
     box.appendChild(deleteButton);
+    box.appendChild(issueButton);
 
     newDiv.appendChild(projectName);
     newDiv.appendChild(projectImage);
     newDiv.appendChild(box);
     projectList.appendChild(newDiv);
     addDeleteProjectListener(newDiv, project.id, deleteButton);
+    addEditProjectListener(newDiv, project.id, editButton);
+    
+
 }
+
 
 function addDeleteProjectListener(listElement, projectId, deleteButton){
     deleteButton.addEventListener('click',  async (event) => {
@@ -87,5 +105,38 @@ function addDeleteProjectListener(listElement, projectId, deleteButton){
         projectList.removeChild(listElement);
     })
 }
+
+
+
+function addEditProjectListener(listElement, projectId, editButton){
+    
+
+projectEditForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    
+    const data = new FormData(projectForm);
+
+    try{
+        const result = await fetch(`http://localhost:3000/api/project/${projectId}`, {
+            method: "PUT",
+            body: data
+        })
+        const json = await result.json();
+       
+
+    }catch(error){
+        alert("Invalid project data");
+        console.error(error);
+       
+    }
+$('#editModal').modal("toggal");
+   // $('#modal fade').modal("close");
+
+
+});
+
+}
+
 
 displayProjects();
